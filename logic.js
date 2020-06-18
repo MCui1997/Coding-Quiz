eb7a068ce06147ee2418132e198a4f1e1c05b0b1
Michael Cui <cuimichael500@gmail.com>
2020-06-18 12:20:57 -0500

    Fixed files with saving highscores

--- logic.js
+++ logic.js
@@ -149,6 +149,7 @@ function saveHighscore() {
 
     // redirect to next page
     window.location.href = "highscores.html";
+    
   }
 }
 
@@ -162,6 +163,7 @@ function checkForEnter(event) {
 // user clicks button to submit initials
 submitBtn.onclick = saveHighscore;
 
+
 // user clicks button to start quiz
 startBtn.onclick = startQuiz;
 
@@ -169,34 +171,3 @@ initialsEl.onkeyup = checkForEnter;
 
 
 
-///// Functions for printing highscores
-
-function printHighscores() {
-  // either get scores from localstorage or set to empty array
-  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-
-  // sort highscores by score property in descending order
-  highscores.sort(function(a, b) {
-    return b.score - a.score;
-  });
-
-  highscores.forEach(function(score) {
-    // create li tag for each high score
-    var liTag = document.createElement("li");
-    liTag.textContent = score.initials + " - " + score.score;
-
-    // display on page
-    var olEl = document.getElementById("highscores");
-    olEl.appendChild(liTag);
-  });
-}
-
-function clearHighscores() {
-  window.localStorage.removeItem("highscores");
-  window.location.reload();
-}
-
-document.getElementById("clear").onclick = clearHighscores;
-
-// run function when page loads
-printHighscores();
